e1eeb58b14a4859a146c8945a2a4f068273d7218
Kaleb Wade <kaleb_wade@mckinsey.com>
2023-10-26 10:08:03 -0500

    Trying different module approach

--- terraform/main.tf
+++ terraform/main.tf
@@ -25,3 +25,15 @@
 
 #   account_customizations_name = "developer-sandbox"
 # }
+
+
+
+
+
+
+
+# module "sandbox-aft-test-03" {
+#     source = "./modules/template"
+#     account_name = "sandbox-aft-test-03"
+#     account_email = "kaleb_wade+${var.account_name}@mckinsey.com"
+# }
