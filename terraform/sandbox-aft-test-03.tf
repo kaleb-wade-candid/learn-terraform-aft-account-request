2f2da85c9a7d29c53847d6186b248b17bb7d2543
Kaleb Wade <kaleb_wade@mckinsey.com>
2023-11-01 13:29:31 -0500

    Comment out all accounts

--- terraform/sandbox-aft-test-03.tf
+++ terraform/sandbox-aft-test-03.tf
@@ -1,27 +1,27 @@
-module "sandbox-aft-test-03" {
-  source = "./modules/aft-account-request"
+# module "sandbox-aft-test-03" {
+#   source = "./modules/aft-account-request"
 
-  control_tower_parameters = {
-    AccountEmail              = "kaleb_wade+sanbox-test-03@mckinsey.com"
-    AccountName               = "sandbox-aft-test-03"
-    ManagedOrganizationalUnit = "Learn AFT"
-    SSOUserEmail              = "kaleb_wade@mckinsey.com"
-    SSOUserFirstName          = "Sandbox"
-    SSOUserLastName           = "AFT"
-  }
+#   control_tower_parameters = {
+#     AccountEmail              = "kaleb_wade+sanbox-test-03@mckinsey.com"
+#     AccountName               = "sandbox-aft-test-03"
+#     ManagedOrganizationalUnit = "Learn AFT"
+#     SSOUserEmail              = "kaleb_wade@mckinsey.com"
+#     SSOUserFirstName          = "Sandbox"
+#     SSOUserLastName           = "AFT"
+#   }
 
-  account_tags = {
-    "Learn Tutorial" = "AFT-01"
-  }
+#   account_tags = {
+#     "Learn Tutorial" = "AFT-01"
+#   }
 
-  change_management_parameters = {
-    change_requested_by = "Kaleb Wade"
-    change_reason       = "Test account deployment via AFT"
-  }
+#   change_management_parameters = {
+#     change_requested_by = "Kaleb Wade"
+#     change_reason       = "Test account deployment via AFT"
+#   }
 
-  custom_fields = {
-    group = "non-prod"
-  }
+#   custom_fields = {
+#     group = "non-prod"
+#   }
 
-  account_customizations_name = "developer-sandbox"
-}
+#   account_customizations_name = "developer-sandbox"
+# }
